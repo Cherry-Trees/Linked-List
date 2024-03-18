a2e426af8b9f08597747b0656266836c579e0133
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2024-03-18 01:31:31 -0500

    Update LinkedList.h

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -1,28 +1,13 @@
+#pragma once
 #include <iostream>
+#include "Node.hpp"
 
 template<typename T> class LinkedList
 {       
 
-    private:
-
-        struct Node
-        {
-            T element;
-            Node* next;
-        };
-
-        // Double Node pointer points to the first Node pointer in this list.
-        Node** _begin; 
-
-        // Double Node pointer points to the last Node pointer in this list.
-        Node** _end;
-
-        // Number of nodes in this list.
-        int _size;
-
-        
+    
     public:
-        
+
         /**
          * Constructor.
          * Initialize size of list to 0, and initialize _begin and _end to new Node pointers.
@@ -34,16 +19,22 @@ template<typename T> class LinkedList
         */
         LinkedList(const LinkedList<T>& list_copy);
 
+        /**
+         * Destructor.
+         * Delete all of the Nodes in the list.
+        */
+        ~LinkedList();
+
 
         T& beginElement() const;
 
-        Node* beginNode() const;
+        Node<T>* beginNode() const;
 
         // O(1)
         T& endElement() const;
 
         // O(1)
-        Node* endNode() const;
+        Node<T>* endNode() const;
 
         // O(n)
         T& operator[](const int index);
@@ -52,18 +43,51 @@ template<typename T> class LinkedList
 
         // Add a Node to the end of the list.
         // O(1) Constant time due to the _end double Node pointer.
-        LinkedList<T>& push(const T val);
+        LinkedList<T>& push_back(const T val);
 
-        // Add a list to this list.
+        // Add a list to the end of this list.
         // This does NOT create a copy, so modifying the original list modifies this one too.
         // Creates a circular list if the same list is added.
         // O(1)
-        LinkedList<T>& push(const LinkedList<T>& list);
 
-        // Add a COPY of a list to this list.
+        // Add a Node to the end of the list.
+        LinkedList<T>& push_back(Node<T>* node);
+
+
+        LinkedList<T>& push_back(const LinkedList<T>& list);
+
+
+        // Add a COPY of a list to the end of this list.
         // O(n)
-        LinkedList<T>& push_copy(const LinkedList<T>& list);
+        LinkedList<T>& push_back_copy(const LinkedList<T>& list);
 
+        // Add a Node to the front of this list.
+        LinkedList<T>& push_front(const T val);
+
+        // Add a Node to the front of the list.
+        LinkedList<T>& push_front(Node<T>* node);
+
+        // Add a list to the front of this list.
+        // This does NOT create a copy, so modifying the original list modifies this one too.
+        // Creates a circular list if the same list is added.
+        // O(1)
+        LinkedList<T>& push_front(const LinkedList<T>& list);
+
+        // Add a COPY of a list to the front of this list.
+        // O(n)
+        LinkedList<T>& push_front_copy(const LinkedList<T>& list);
+
+        // O(n)
+        LinkedList<T>& insert(const int index, const T val);
+        LinkedList<T>& insert(const int index, Node<T>* node);
+        LinkedList<T>& insert(const int index, const LinkedList<T>& list);
+
+        // O(1) Given a reference node.
+        LinkedList<T>& insert(Node<T>* prev_node, const T val);
+        LinkedList<T>& insert(Node<T>* prev_node, Node<T>* node);
+        LinkedList<T>& insert(Node<T>* prev_node, const LinkedList<T>& list);
+
+        LinkedList<T>& insert_copy(Node<T>* prev_node, const LinkedList<T>& list);
 
         // Add a COPY of a list to this list.
         // O(n)
@@ -74,6 +98,24 @@ template<typename T> class LinkedList
         LinkedList<T>& operator*=(const int scalar);
 
 
+        void printNodes();
+
+
+
         template<typename _T> friend std::ostream& operator<<(std::ostream& out, const LinkedList<_T>& ll);
+    
+
+    private:
+
+        
+
+        // Double Node pointer points to the first Node pointer in this list.
+        Node<T>** _begin; 
+
+        // Double Node pointer points to the last Node pointer in this list.
+        Node<T>** _end;
+
+        // Number of nodes in this list.
+        int _size;
 
 };
